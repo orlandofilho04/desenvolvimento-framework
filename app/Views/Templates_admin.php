610dc4fabcc3c637e8b7c2beed7d82b47cf7c87a
Orlando Filho <orlandossf04@gmail.com>
2025-04-09 19:15:46 -0300

    sai fora

--- app/Views/Templates_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    include('templates/header.php');
-    include('templates/nav.php');
-    echo $this->renderSection('content');
-    include('templates/footer.php');
-    include('templates/end.php');
-    
-    ?>
--- app/Views/templates/footer.php
+++
@@ -1,4 +0,0 @@
-<br>
-<div class="align-items-center text-center container-fluid bg-dark text-light">
-    Todos os direitos reservados
-</div>
